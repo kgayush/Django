4e0669b7a28523b2a45200bb4336376f1a40dd19
agga1211 <54178133+agga1211@users.noreply.github.com>
2022-01-23 23:38:47 +0530

    Add HTML files

--- lecture3/hello/views.py
+++ lecture3/hello/views.py
@@ -5,7 +5,7 @@ from django.shortcuts import render
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hello World!")
+    return render(request, "hello/index.html")
 
 def brian(request):
     return HttpResponse("Hello Brian!")
@@ -14,4 +14,6 @@ def david(request):
     return HttpResponse("Hello David!")
 
 def greet(request, name):
-    return HttpResponse(f"Hello {name}!")
+    return render(request, "hello/greet.html", {
+        "name": name.capitalize()
+    })
