c8a088a274d80429c8d0a42b8626f7d5c70ef4a0
agga1211 <54178133+agga1211@users.noreply.github.com>
2022-01-24 23:28:26 +0530

    Created airline project

---
+++ airline/flights/models.py
@@ -0,0 +1,9 @@
+from operator import mod
+from django.db import models
+
+# Create your models here.
+
+class Flight(models.Model):
+    origin = models.CharField(max_length=64)
+    destination = models.CharField(max_length=64)
+    duration = models.IntegerField
