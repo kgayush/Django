d87b858bfe23d5c00c271589e13b89256d1464d9
agga1211 <54178133+agga1211@users.noreply.github.com>
2022-01-25 00:25:26 +0530

    Created database

--- airline/flights/migrations/0001_initial.py
+++ airline/flights/migrations/0001_initial.py
@@ -17,6 +17,7 @@ class Migration(migrations.Migration):
                 ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
                 ('origin', models.CharField(max_length=64)),
                 ('destination', models.CharField(max_length=64)),
+                ('duration', models.IntegerField()),
             ],
         ),
     ]
