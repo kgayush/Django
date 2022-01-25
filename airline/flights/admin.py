548d7cc7d1f693bf19e519cecc35cd4fe277211c
agga1211 <54178133+agga1211@users.noreply.github.com>
2022-01-25 22:02:13 +0530

    Added passenger booking

--- airline/flights/admin.py
+++ airline/flights/admin.py
@@ -1,8 +1,20 @@
 from django.contrib import admin
 
-from .models import Airport, Flight
+from .models import *
 
 # Register your models here.
 
-admin.site.register(Airport)
-admin.site.register(Flight)
+class FlightAdmin(admin.ModelAdmin):
+    list_display = ("id", "origin", "destination", "duration")
+
+class AirportAdmin(admin.ModelAdmin):
+    list_display = ("id", "city", "code")
+
+class PassengerAdmin(admin.ModelAdmin):
+    list_display = ("id", "first", "last")
+    filter_horizontal = ("flights",)
+
+
+admin.site.register(Airport, AirportAdmin)
+admin.site.register(Flight, FlightAdmin)
+admin.site.register(Passenger, PassengerAdmin)
